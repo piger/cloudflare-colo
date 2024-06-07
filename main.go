b8f625cda86f9dfa834fe29e5ba85d167f9572ae
Daniel Kertesz <daniel@spatof.org>
2024-06-07 23:00:25 +0100

    shorten and simplify CSS selectors

--- main.go
+++ main.go
@@ -52,10 +52,6 @@ func parseLocations() ([]ColoLocation, error) {
 		return nil, err
 	}
 
-	for _, loc := range locations {
-		fmt.Printf("%+v\n", loc)
-	}
-
 	return locations, nil
 }
 
@@ -79,46 +75,12 @@ func run() error {
 		return err
 	}
 
-	/*
-		doc.Find("div.component-container.border-color.is-group.open").Each(func(i int, s *goquery.Selection) {
-			// fmt.Println("found big section")
-
-			s.Find("div.component-inner-container").Each(func(ii int, ss *goquery.Selection) {
-				// fmt.Println("found inner section")
-
-				ss.Find("span.name").Each(func(_ int, sss *goquery.Selection) {
-					sss.Find("span").Each(func(_ int, thing *goquery.Selection) {
-						if thing.HasClass("font-small") {
-							return
-						}
-						fmt.Printf("%s\n", sss.Text())
-					})
-				})
-			})
-		})
-
-		doc.Find("div.component-container > div.component-inner-container > span.name > span").Each(func(i int, s *goquery.Selection) {
-			if s.HasClass("font-small") {
-				return
-			}
-			fmt.Printf("- %s\n", strings.TrimSpace(s.Text()))
-		})
-	*/
-
 	colos := make(map[string]Colo)
 
 	doc.Find("div.component-container").Each(func(i int, s *goquery.Selection) {
-		var continent string
-
-		s.Find("div.component-inner-container > span.name > span").Each(func(i int, s *goquery.Selection) {
-			// continent
-			if s.HasClass("font-small") {
-				return
-			}
-
-			continent = strings.TrimSpace(s.Text())
-		})
-
+		continent := strings.TrimSpace(
+			s.Find(`div.component-inner-container > span.name > span:not([class~="font-small"])`).Text(),
+		)
 		if continent == "Cloudflare Sites and Services" {
 			return
 		}
@@ -127,10 +89,8 @@ func run() error {
 
 		s.Find("div.child-components-container > div.component-inner-container > span.name").Each(func(i int, s *goquery.Selection) {
 			colo := strings.TrimSpace(s.Text())
-			fmt.Printf("- %s\n", colo)
-
 			where, iata := splitColoString(colo)
-			fmt.Printf("foo=%q, bar=%q\n", where, iata)
+			fmt.Printf("- colo=%q where=%q, iata=%q\n", colo, where, iata)
 			colos[iata] = Colo{Name: where, Iata: iata, Continent: continent}
 		})
 	})
